6049caada9fad2b3004cb01deb2ac7923e2f761d
chayanthithan <kumarchayan31@gmail.com>
2024-02-28 20:13:23 +0530

    i have added some code

--- day3.js
+++ day3.js
@@ -7,3 +7,16 @@ let name="kumar";
 //concat
 console.log("hello my name is:"+name);
 console.log(`my name is ${name}`);
+
+//length
+//lastIndexOf();
+//substring
+//chatAt
+//trim
+//trimEnd
+//trimStart
+//startsWith
+//replace
+//includes---to check a particular element is available or not
+// const newString=new String(name);
+//split
