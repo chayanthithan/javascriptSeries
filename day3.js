abface45cc73da8e338a70083fb56636853c8f00
chayanthithan <kumarchayan31@gmail.com>
2024-02-29 19:07:50 +0530

    new questions

--- day3.js
+++ day3.js
@@ -25,11 +25,17 @@
 //toFixed(2) feter decimal value
 //toPrecision(4)- if .5 it chenge before value to 1
 //Math.random();
-let max=20;
-let min=10;
 
-let result=Math.floor(Math.random()*(max-min+1))
-console.log(result);    //it's print value without floating value
+// let max=20;
+// let min=10;
 
-let i =-10;
-console.log(Math.abs(i))  //change - to +
+// let result=Math.floor(Math.random()*(max-min+1))
+// console.log(result);    //it's used to print value without floating value
+
+// let i =-10;
+// console.log(Math.abs(i))  //change - to +
+
+let var3="12";
+console.log(var3*2);
+console.log(Math.floor(3.02))
+console.log(Math.ceil(3.52))
