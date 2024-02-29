7028deeacd1d66908f9bf41a1423d32457a4154d
chayanthithan <kumarchayan31@gmail.com>
2024-02-29 08:28:34 +0530

    new changes

--- day3.js
+++ day3.js
@@ -1,12 +1,12 @@
-const name1="chayan";
-let name2=""
+// const name1="chayan";
+// let name2=""
 
-// String
-let name="kumar";
+// // String
+// let name="kumar";
 
-//concat
-console.log("hello my name is:"+name);
-console.log(`my name is ${name}`);
+// //concat
+// console.log("hello my name is:"+name);
+// console.log(`my name is ${name}`);
 
 //length
 //lastIndexOf();
@@ -20,3 +20,16 @@ console.log(`my name is ${name}`);
 //includes---to check a particular element is available or not
 // const newString=new String(name);
 //split
+//toString
+//abs---to round
+//toFixed(2) feter decimal value
+//toPrecision(4)- if .5 it chenge before value to 1
+//Math.random();
+let max=20;
+let min=10;
+
+let result=Math.floor(Math.random()*(max-min+1))
+console.log(result);    //it's print value without floating value
+
+let i =-10;
+console.log(Math.abs(i))  //change - to +
